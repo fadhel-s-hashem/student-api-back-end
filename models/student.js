d9b8daf8fc414ade7222a5486d6a73b935feab0f
Fadhel <fadhel.s.hashem@gmail.com>
2026-08-02 09:47:21 +0300

    gneral setup and student schema

---
+++ models/student.js
@@ -0,0 +1,26 @@
+const mongoose= require('mongoose')
+
+const studentSchema = new mongoose.Schema({
+
+    name : {
+        type: String,
+      required: true,
+      trim: true,
+    },
+    
+    favorriteFood: {
+        type: String,
+      default: 'Not added yet',
+      trim: true,
+    },
+
+    favoriteEmoji: {
+      type: String,
+      default: '🙂',
+      trim: true,
+    },
+}, {timestamps: true})
+
+const Student = mongoose.model('Student', studentSchema)
+
+module.exports = Student
---
+++ server.js
@@ -0,0 +1,41 @@
+// server.js
+
+require('dotenv').config()
+
+const dns = require("node:dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors') // new one
+
+// the conttrolers
+const studentCtrl = require('./controllers/students.js')
+
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name} 🥭`)
+})
+
+// this line allows our React Front End permission to connect to our backend
+app.use(cors({ origin: 'http://localhost:5173' })) // new one
+
+app.use(express.json())// also new
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the Students API' })
+})
+
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`The express app is running on port ${port}`)
+})
